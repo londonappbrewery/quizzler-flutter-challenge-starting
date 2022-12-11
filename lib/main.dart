8c475610614fa8e752e4ef578ba70386c763da62
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2022-12-11 18:27:31 +0100

    Adding TODO

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,18 @@ class _QuizPageState extends State<QuizPage> {
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
     setState(() {
+    
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+
       if (userPickedAnswer == correctAnswer) {
+
+      
         print('user got it right!');
         scoreKeeper.add(Icon(
           Icons.check,
